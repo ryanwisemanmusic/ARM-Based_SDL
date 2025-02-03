df6da50616bfb15d887df9c3b5064e2178f1263b
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2025-02-03 12:33:32 -0800

    Our furry main
    The most fur way of announcing that you are successful in your headers being found. Now go, learn SDL2

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <iostream>
+
+int main()
+{
+    /*I am not changing this, this is
+    non negotiable*/
+    printf ("UwU, you did it!");
+}
